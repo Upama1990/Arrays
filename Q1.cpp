79632c12cb15dc2bd889ba1571cae18d97f7c55e
Upama Roy <upama.roy09@gmail.com>
2022-09-12 23:29:09 +0200

    adding data inside Q1

--- Q1.cpp
+++ Q1.cpp
@@ -3,24 +3,31 @@ using namespace std;
 int main()
 {
 
-    int s , i;
+    int s ;
     cout<<endl<<"Enter the size of array :" ;
     cin>>s;
+    
 
-    int a[s];
+    int a[s], i, j, r[s];
+    j=s-1;
+   
 
     for(i=0; i<s ; i++)
     {
       cout<<endl<<"Enter the elements of array : ";
        cin>>a[i];
-    }
+
+       r[j--] = a[i];
+
        
-    for(i=(s-1) ; i >= 0 ; i--)   
-    {
-       cout<<endl<<"a ["<<i<<"] = " <<a[i];
-      
-        
+
     }
+
+    for(i=0;i<s;i++)
+    
+     cout<<endl<<a[i]<<"   "<<r[i];
+   
+    
 
  return 0;
 
